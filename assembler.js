f91015a87f85f71688ec60d76b9fb0dc983ab39f
vansh-shriv <vanshsvst@gmail.com>
2025-03-17 23:07:56 +0530

    opcode fixing

--- assembler.js
+++ assembler.js
@@ -1,25 +1,25 @@
 const INSTRUCTION_SET = {
-    'ADD': '00000',
-    'SUB': '00001',
-    'AND': '00010',
-    'OR': '00011',
-    'XOR': '00100',
-    'NOT': '00101',
-    'MOV': '00110',
-    'LSL': '00111',
-    'LSR': '01000',
-    'ASR': '01001',
-    'CMP': '01010',
-    'JMP': '01011',
-    'JEQ': '01100',
-    'JNE': '01101',
-    'LD': '01110',
-    'ST': '01111',
-    'B': '10100',
-    'BL': '10101',
-    'RET': '10110',
-    'PUSH': '10111',
-    'POP': '11000'
+    'ADD': '00000','add': '00000',
+    'SUB': '00001','sub': '00001',
+    'MUL': '00010','mul': '00010',
+    'DIV': '00011','div': '00011',
+    'MOD': '00100','mod': '00100',
+    'CMP': '00101','cmp': '00101',
+    'AND': '00110','and': '00110',
+    'OR': '00111','or': '00111',
+    'NOT': '01000','not': '01000',
+    'MOV': '01001','mov': '01001',
+    'LSL': '01010','lsl': '01010',
+    'LSR': '01011','lsr': '01011',
+    'ASR': '01100','asr': '01100',
+    'NOP': '01101','nop': '01101',
+    'LD': '01110','ld': '01110',
+    'ST': '01111','st': '01111',
+    'BEQ': '10100','beq': '10100',
+    'BGT': '10101','bgt': '10101',
+    'B': '10110','b': '10110',
+    'CALL': '10111','call': '10111',
+    'RET': '11000','ret': '11000'
 };
 
 function parseLine(line) {
@@ -49,7 +49,8 @@ function instructionToMachineCode(command, argumentsList) {
         arg = String(arg).replace(/,$/, ''); // Ensure arg is a string and remove trailing commas
         if (arg.startsWith('R')) {
             binaryArgs += parseInt(arg.slice(1)).toString(2).padStart(5, '0'); // Convert register (e.g., R1) to binary
-        } else {
+        }  
+         else {
             binaryArgs += parseInt(arg).toString(2).padStart(5, '0'); // Convert immediate values or label addresses to binary
         }
     }
@@ -60,7 +61,7 @@ export function assemble(script) {
     const machineCode = [];
     const symbolTable = {}; // Symbol table to store label addresses
     const lines = script.split('\n');
-    let currentAddress = 0;
+    let currentAddress = '0000000000';
 
     // First Pass: Build the symbol table
     for (let i = 0; i < lines.length; i++) {
@@ -75,9 +76,6 @@ export function assemble(script) {
             }
         }
     }
-
-    console.log(symbolTable);
-
     // Second Pass: Generate machine code
     for (let i = 0; i < lines.length; i++) {
         const parsed = parseLine(lines[i]);
@@ -99,37 +97,34 @@ export function assemble(script) {
             }
         }
     }
-
     return machineCode.join('\n');
 }
 
 // Examples for testing
-console.log(parseLine("LOOP: ADD R1, R2  # A comment"));
+//console.log(parseLine("LOOP: ADD R1, R2  # A comment"));
 // Output: ['LOOP', 'ADD', ['R1,', 'R2']]
 
-console.log(parseLine("MOV R3, R4"));
+//console.log(parseLine("MOV R3, R4"));
 // Output: [null, 'MOV', ['R3,', 'R4']]
 
-console.log(parseLine("JUMP:  # Just a label"));
+//console.log(parseLine("JUMP:  # Just a label"));
 // Output: ['JUMP', null, null]
 
-console.log(parseLine("# This is a comment"));
+//console.log(parseLine("# This is a comment"));
 // Output: null
 
 const script1 = `
-LOOP: ADD R1, R2  # A comment
-MOV R3, R4
+LOOP: add R1, R2  # A comment
+mov R3, R4
 JUMP:  # Just a label
 # This is a comment
 `;
 
-
-
 const script2 = `
 START:  MOV R1, 1     # Load 1 into R1  
         CMP R1, R2    # Compare R1 and R2  
-        JEQ EQUAL     # Jump if equal  
-        JMP END       # Unconditional jump  
+        BEQ EQUAL     # Jump if equal  
+        B  END       # Unconditional jump  
 EQUAL:  MOV R0, 1     # Set R0 to 1 if equal  
 END:    RET           # End of program
 `;
@@ -139,7 +134,7 @@ console.log(assemble(script1));
 // 0000000000100010
 // 001100011000100
 
-console.log(assemble(script2));
+// console.log(assemble(script2));
 // Expected Output:
 // 001100000100001
 // 010100000100010
